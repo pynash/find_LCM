66c14403b4ea7af23ccd3e8165b0568de711d584
pynash <pynash@gmail.com>
2017-10-09 20:53:18 -0600

    add read number

--- find_LCM.py
+++ find_LCM.py
@@ -7,27 +7,37 @@ def minmax(a, b):
     return x, y
 
 def factors(a, b):
-    if a%2==0:
-        a = a/2
-        if b%2==0:
-            b = b/2
-            print a, b
-            # return a, b, 2
+    if a>1:
+        if a%2==0:
+            a = a/2
+            if b%2==0 and b>1:
+                b = b/2
+                print a, b
+                # return a, b, 2
+            else:
+                print a, b, 2
+                # return a, b, 2
         else:
-            print a, b, 2
-            # return a, b, 2
+            if b%a==0:
+                b = b/a
+                c = a
+                a = a/a
+                print a, b
+                # return a, b, c
     else:
-        if b%a==0:
-            b = b/a
-            c = a
-            a = a/a
-            print a, b
-            # return a, b, c
+
+def read_number():
+    while True:
+        x = int(raw_input("please, enter number up 1: "))
+        if x>1:
+            return x
 
 def main():
-    x1 = int(raw_input("please, enter the First Number: "))
-    x2 = int(raw_input("please, enter the Second Number: "))
+    x1 = read_number()
+    x2 = read_number()
     a, b = minmax(x1, x2)
+
+
 
     factors(x1, x2)
 
